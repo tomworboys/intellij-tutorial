47acafeb9591cb825821d6aa303a214bffe54372
Tom Worboys <tomworboys@gmail.com>
2022-03-22 09:32:54 +0000

    Another commit

--- src/test/java/taudemo/SeleniumTest.java
+++ src/test/java/taudemo/SeleniumTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
